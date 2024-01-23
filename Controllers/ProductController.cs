f6bb84cda7a756c51ccd8d82ef399202dfc5f0d0
Administrator <Administrator@DESKTOP-TLSKMTQ>
2024-01-23 14:33:47 +0530

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductMVC.Models;
+
+namespace ProductMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<Product> products = new List<Product>();
+
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                products.Add(product);
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var product = products.Find(p => p.ProductId == id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingProduct = products.Find(p => p.ProductId == product.ProductId);
+                existingProduct.ProductName = product.ProductName;
+                existingProduct.ProductDescription = product.ProductDescription;
+
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+public class Product
+{
+    private static int nextId = 1;
+
+    [Key]
+    public int ProductId { get; set; }
+
+    [Required(ErrorMessage = "Product Name is required.")]
+    public string ProductName { get; set; }
+
+    [MaxLength(100, ErrorMessage = "Product Description cannot exceed 100 characters.")]
+    public string ProductDescription { get; set; }
+
+    public Product()
+    {
+        ProductId = nextId++;
+    }
+}
